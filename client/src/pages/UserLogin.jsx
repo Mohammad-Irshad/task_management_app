bfb143db1d52ad4b525906e3d665582b18392d3a
Mohammad-Irshad <132482951+Mohammad-Irshad@users.noreply.github.com>
2025-01-20 17:28:10 +0530

    Update UserLogin.jsx

--- client/src/pages/UserLogin.jsx
+++ client/src/pages/UserLogin.jsx
@@ -48,8 +48,8 @@ const UserLogin = () => {
                         {errorMessage && <p>{errorMessage}</p>}
                     </form>   
                     <br/>
-                    <Link to={'/signup'}>Don't have an account? Click here to singup.</Link> 
                     <p>Click Login! To Login as Guest User.</p>
+                    <Link to={'/signup'}>Don't have an account? Click here to singup.</Link> 
                 </div>
             </div>
         </div>
